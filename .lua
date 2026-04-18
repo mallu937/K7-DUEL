2f137b5b22d80d02172e45204c16b44812cf0ced
mallu937 <appuappu0001112@gmail.com>
2026-04-18 14:52:46 +0530

    Update .lua

--- .lua
+++ .lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.cc/files/v4/loaders/8e7b0bcdeaf24f70a7a51f2d93c4ec3b.lua"))()
+loadstring(game:HttpGet("https://api.pastefy.lol/files/v4/loaders/8e7b0bcdeaf24f70a7a51f2d93c4ec3b.lua"))()
